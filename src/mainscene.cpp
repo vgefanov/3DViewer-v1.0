9a476e92733b196bb78008f29a0a0a7165ddfe95
Farfetch Adelia <farfetch@in-f1.nsk.21-school.ru>
2022-07-25 12:38:41 +0700

    220725: finale check

--- src/mainscene.cpp
+++ src/mainscene.cpp
@@ -1,12 +1,36 @@
+#include <QFile>
 #include <iostream>
 #include "mainscene.h"
 
+float default_vertexes[24] = { 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1,
+                               1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1 };
+unsigned int default_faces[72] = { 0, 6, 6, 4, 4, 0, 0, 2, 2, 6, 6, 0, 0, 3, 3, 2, 2, 0,
+                                   0, 1, 1, 3, 3, 0, 2, 7, 7, 6, 6, 2, 2, 3, 3, 7, 7, 2,
+                                   4, 6, 6, 7, 7, 4, 4, 7, 7, 5, 5, 4, 0, 4, 4, 5, 5, 0,
+                                   0, 5, 5, 1, 1, 0, 1, 5, 5, 7, 7, 1, 1, 7, 7, 3, 3, 1 };
+
 MainScene::MainScene()
 {
-    // Настройки
-    sceneSettings = new SceneSettings("/Users/farfetch/C8_3DViewer_v1.0-0/src/models/medium/al.obj");
+    QString filename = "/Users/farfetch/C8_3DViewer_v1.0-0/src/models/medium/al.obj";
+    QFile file(filename);
+    if (file.open(QIODevice::ReadOnly)) {
+        sceneSettings = new SceneSettings(filename);
+        model = load_model(sceneSettings->modelPath.toLocal8Bit().data());
+    } else {
+        sceneSettings = new SceneSettings("models/small/cube.obj");
+        model.vertexes = default_vertexes;
+        model.faces = default_faces;
+        model.v_num = 24;
+        model.f_num = 72;
+        model.x_min = 0;
+        model.x_max = 1;
+        model.y_min = 0;
+        model.y_max = 1;
+        model.z_min = 0;
+        model.z_max = 1;
+    }
+    file.close();
 
-    model = load_model(sceneSettings->modelPath.toLocal8Bit().data());
     // размеры проекции
     projection_min = (model.x_min < model.y_min) ? model.x_min : model.y_min;
     if (model.z_min < projection_min) projection_min = model.z_min;
